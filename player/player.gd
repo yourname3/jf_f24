6b4d0ce9670e7fae6b95b864bbaa1152f11c5f3c
ben <bwall4@gmu.edu>
2024-11-09 21:21:31 -0500

    Implement basic spikes

--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,7 @@ const H_ACCEL = 1400.0
 const SPEED = 256.0 * 6
 const JUMP_VELOCITY = -2000.0
 
+@onready var sprite = $Sprite
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,13 +15,23 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Horizontal acceleration
 	var direction = Input.get_axis("player_left", "player_right")
 	var target_speed = direction * SPEED
 	var accel = H_ACCEL
 	if sign(velocity.x) != sign(target_speed):
 		accel *= 2
 	velocity.x = move_toward(velocity.x, target_speed, accel * delta)
+	
+	if abs(velocity.x) > SPEED * 0.03:
+		$Sprite.scale.x = sign(velocity.x)
 
 	move_and_slide()
+
+func die():
+	#get_tree().change_scene_to_packed(get_tree().current_scene)
+	get_tree().reload_current_scene()
+
+func _on_hazard_body_entered(body):
+	# Player dies.
+	die()
