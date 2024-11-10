c6ff929ed3f240ed5363cc16f38e2915f2955744
ben <bwall4@gmu.edu>
2024-11-09 22:46:29 -0500

    More camera limits

--- camera/game_camera.gd
+++ camera/game_camera.gd
@@ -7,8 +7,19 @@ func go(delta):
 	global_position = player.global_position
 	
 	var half_y = get_viewport().size.y * 0.5 / zoom.y
+	var half_x = get_viewport().size.x * 0.5 / zoom.x
 	
 	for limit in get_tree().get_nodes_in_group("CameraLowestY"):
 		var limit_pos = limit.global_position.y - half_y
 		if global_position.y > limit_pos:
 			global_position.y = limit_pos
+
+	for limit in get_tree().get_nodes_in_group("CameraRightmost"):
+		var limit_pos = limit.global_position.x - half_x
+		if global_position.x > limit_pos:
+			global_position.x = limit_pos
+
+	for limit in get_tree().get_nodes_in_group("CameraLeftmost"):
+		var limit_pos = limit.global_position.x + half_x
+		if global_position.x < limit_pos:
+			global_position.x = limit_pos
