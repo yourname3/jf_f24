8287b6610a827844b86c7811eb96f00201e48286
ben <bwall4@gmu.edu>
2024-11-10 21:14:37 -0500

    fullscreen

--- globals.gd
+++ globals.gd
@@ -2,3 +2,10 @@ extends Node
 
 var Gallery = preload("res://gallery/gallery_level.tscn")
 var MainMenu = preload("res://title/title.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
